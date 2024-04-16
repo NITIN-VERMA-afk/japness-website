8c30d557b45e17814e6eb7618fa3567bab3c33f1
Nitin Verma <nitinvermanv61506@gmail.com>
2024-04-16 17:29:20 +0530

    gg

--- src/app/Components/Services.jsx
+++ src/app/Components/Services.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Services = () => {
   return (
     <div>
-         <section className="py-16">
+         <section id='services' className="py-16">
         <div className="container mx-auto px-4">
           
           <h2 className="text-2xl sm:text-3xl font-bold mb-4">Our Services</h2>
